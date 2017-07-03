5bf2cc7b8a398d9fc6d3daf6031b811a06b7556e
ics-ikeda <ikeda@ics-web.jp>
2017-07-03 22:20:12 +0900

    TypeScript+Three.jsのサンプルを追加

---
+++ tutorial-typescript-three/src/main.ts
@@ -0,0 +1,41 @@
+import * as THREE from 'three';
+
+window.addEventListener('DOMContentLoaded', () => {
+  // レンダラーを作成
+  const renderer = new THREE.WebGLRenderer();
+  // レンダラーのサイズを設定
+  renderer.setSize(800, 600);
+  // canvasをbodyに追加
+  document.body.appendChild(renderer.domElement);
+
+  // シーンを作成
+  const scene = new THREE.Scene();
+
+  // カメラを作成
+  const camera = new THREE.PerspectiveCamera(45, 800 / 600, 1, 1000);
+
+  // 箱を作成
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshPhongMaterial({color: 0xff0000});
+  const box      = new THREE.Mesh(geometry, material);
+  box.position.z = -5;
+  scene.add(box);
+
+  // 平行光源を生成
+  const light = new THREE.DirectionalLight(0xffffff);
+  light.position.set(1, 1, 1);
+  scene.add(light);
+
+  const tick = (): void => {
+    requestAnimationFrame(tick);
+
+    box.rotation.x += 0.05;
+    box.rotation.y += 0.05;
+
+    // 描画
+    renderer.render(scene, camera);
+  };
+  tick();
+
+  console.log('Hello Three.js');
+});
---
+++ tutorial-typescript-three/webpack.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  // メインとなるJavaScriptファイル（エントリーポイント）
+  entry: './src/main.ts',
+  // ファイルの出力設定
+  output: {
+    //  出力ファイルのディレクトリ名
+    path: `${__dirname}/build`,
+    // 出力ファイル名
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        // 拡張子 .ts の場合
+        test: /\.ts$/,
+        // TypeScript をコンパイルする
+        use: 'awesome-typescript-loader'
+      },
+      // ソースマップファイルの処理
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'source-map-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: [
+      '.ts', '.js', '.json'
+    ],
+  },
+  // ソースマップを有効に
+  devtool: 'source-map'
+};
