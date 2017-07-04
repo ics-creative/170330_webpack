e1b5eb1876e9d5e10f178fe44cce27467da6a37b
ics-ikeda <ikeda@ics-web.jp>
2017-07-04 13:22:19 +0900

    TypeScriptのサンプルが壊れていたので調整

--- tutorial-typescript/webpack.config.js
+++ tutorial-typescript/webpack.config.js
@@ -11,43 +11,8 @@ module.exports = {
   module: {
     rules: [
       {
-        // 拡張子 .ts の場合module.exports = {
-        // メインとなるJavaScriptファイル（エントリーポイント）
-        entry: './src/main.ts',
-        // ファイルの出力設定
-        output: {
-          //  出力ファイルのディレクトリ名
-          path: `${__dirname}/build`,
-          // 出力ファイル名
-          filename: 'bundle.js'
-        },
-        module: {
-          rules: [
-            {
-              // 拡張子 .ts の場合
-              test: /\.ts$/,
-              // TypeScript をコンパイルする
-              use: 'awesome-typescript-loader'
-            },
-            // ソースマップファイルの処理
-            {
-              enforce: 'pre',
-              test: /\.js$/,
-              loader: 'source-map-loader'
-            }
-          ]
-        },
-        // import 文で .ts ファイルを解決するため
-        resolve: {
-          extensions: [
-            '.ts'
-          ],
-        },
-        // ソースマップを有効に
-        devtool: 'source-map'
-      };
-
-test: /\.ts$/,
+        // 拡張子 .ts の場合
+        test: /\.ts$/,
         // TypeScript をコンパイルする
         use: 'awesome-typescript-loader'
       },
