e0c854fc82376a453f1e76a80e159d3856028b8b
ics-ikeda <ikeda@ics-web.jp>
2018-02-26 00:05:34 +0900

    babel: webpack 3.x => 4.0

--- tutorial-babel/webpack.config.js
+++ tutorial-babel/webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   // ファイルの出力設定
   output: {
     //  出力ファイルのディレクトリ名
-    path: `${__dirname}/build`,
+    path: `${__dirname}/dist`,
     // 出力ファイル名
     filename: 'bundle.js'
   },
@@ -30,7 +30,5 @@ module.exports = {
         ]
       }
     ]
-  },
-  // ソースマップを有効にする
-  devtool: 'source-map'
+  }
 };
