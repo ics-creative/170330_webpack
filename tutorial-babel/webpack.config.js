c5849b143ab841bd14106ae7e22057fc0817b1af
ics-ikeda <ikeda@ics-web.jp>
2018-02-26 17:45:34 +0900

    babel: webpack 3.x => 4.0

--- tutorial-babel/webpack.config.js
+++ tutorial-babel/webpack.config.js
@@ -1,13 +1,11 @@
 module.exports = {
+  // モード値を production に設定すると最適化された状態で、
+  // development に設定するとソースマップ有効でJSファイルが出力される
+  mode: 'production',
+
   // メインとなるJavaScriptファイル（エントリーポイント）
-  entry: './src/main.js',
-  // ファイルの出力設定
-  output: {
-    //  出力ファイルのディレクトリ名
-    path: `${__dirname}/dist`,
-    // 出力ファイル名
-    filename: 'bundle.js'
-  },
+  entry: './src/app.js',
+
   module: {
     rules: [
       {
