fb8003ce81446a6cc3e816c3148ff5f2a699e966
ics-ikeda <ikeda@ics-web.jp>
2018-02-26 18:06:08 +0900

    babel+RxJS : webpack 3.x => 4.0

--- tutorial-babel-rxjs/webpack.config.js
+++ tutorial-babel-rxjs/webpack.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  // モード値を production に設定すると最適化された状態で、
+  // development に設定するとソースマップ有効でJSファイルが出力される
+  mode: 'production',
+
   // メインとなるJavaScriptファイル（エントリーポイント）
   entry: {
     'import_all': './src/import_all.js',
@@ -9,7 +13,7 @@ module.exports = {
   // ファイルの出力設定
   output: {
     //  出力ファイルのディレクトリ名
-    path: `${__dirname}/build`,
+    path: `${__dirname}/dist`,
     // 出力ファイル名
     filename: '[name].js'
   },
@@ -35,7 +39,5 @@ module.exports = {
         ]
       }
     ]
-  },
-  // ソースマップを有効にする
-  devtool: 'source-map'
+  }
 };
