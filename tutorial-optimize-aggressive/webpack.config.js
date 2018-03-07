5fae398eef3f0c40d65ead14f8903ebb7dcc8f45
ics-ikeda <ikeda@ics-web.jp>
2018-03-07 17:05:49 +0900

    webpack最適化のコメントを追加

--- tutorial-optimize-aggressive/webpack.config.js
+++ tutorial-optimize-aggressive/webpack.config.js
@@ -1,18 +1,14 @@
-const webpack = require('webpack');
-
 module.exports = {
 
-  mode: "production",
+  mode: 'production',
 
-  plugins: [
-    // 【効果：中】Scope Hoistingをするためのプラグイン
-    new webpack.optimize.ModuleConcatenationPlugin(),
-    // 【効果：小】モジュールとチャンクのIDを使用(出現)頻度で割り当て、
-    // 頻繁に使用されるIDに、より低い（より短い）IDを割り当てる事で、
-    // 予測可能なIDの作成と、合計ファイルサイズを縮小することができるようになるプラグイン
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    // 【効果：小】既に十分に圧縮されている場合にも詳細にコードを分析し共通化できそうな箇所は
-    // まとめてより積極的にコードを圧縮するためのプラグイン
-    new webpack.optimize.AggressiveMergingPlugin(),
-  ]
+  // webpack 4の最適化は以下の記事が参考になる
+  // https://medium.com/webpack/webpack-4-mode-and-optimization-5423a6bc597a
+  optimization: {
+    // production のときは true になる
+    // uglify-js がデフォルトで使われる。
+    // 別のものを使いたい場合は minimizer を使う
+    minimize: true
+  }
+
 };
