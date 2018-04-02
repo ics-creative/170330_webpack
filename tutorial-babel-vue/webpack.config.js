1b0b11a16cd3534322312fd0d1eacda495caeef5
ics-ikeda <ikeda@ics-web.jp>
2018-04-02 17:01:03 +0900

    babel+vueのサンプルを追加

--- tutorial-babel-vue/webpack.config.js
+++ tutorial-babel-vue/webpack.config.js
@@ -20,6 +20,15 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
+        // Babel のオプションを指定する
+        options: {
+          presets: [
+            // env を指定することで、ES2017 を ES5 に変換。
+            // {modules: false}にしないと import 文が Babel によって CommonJS に変換され、
+            // webpack の Tree Shaking 機能が使えない
+            ['env', {'modules': false}]
+          ]
+        },
         exclude: /node_modules/
       },
     ]
