3f9725a3ce06c1a46a201bf7182a144df81252bc
ics-ikeda <ikeda@ics-web.jp>
2018-04-09 17:32:49 +0900

    Bootstrapサンプルの調整

--- tutorial-bootstrap-style-js/webpack.config.js
+++ tutorial-bootstrap-style-js/webpack.config.js
@@ -1,17 +1,9 @@
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const webpack = require('webpack');
-
-// [定数] webpack の出力オプションを指定します
-// 'production' か 'development' を指定
-const MODE = 'development';
-
-// ソースマップの利用有無(productionのときはソースマップを利用しない)
-const enabledSourceMap = (MODE === 'development');
 
 module.exports = {
   // モード値を production に設定すると最適化された状態で、
   // development に設定するとソースマップ有効でJSファイルが出力される
-  mode: MODE,
+  mode: 'production',
   module: {
     rules: [
       {
@@ -23,12 +15,12 @@ module.exports = {
           {
             loader: 'css-loader',
             options: {
-              // オプションでCSS内のurl()メソッドの取り込みを禁止する
-              url: true,
+              // オプションでCSS内のurl()メソッドの取り込まない
+              url: false,
               // ソースマップの利用有無
-              sourceMap: enabledSourceMap,
-              // プロダクションビルドのときだけ空白文字を取り除く
-              minimize: MODE === 'production',
+              sourceMap: true,
+              // 空白文字を取り除く
+              minimize: true,
               // Sass+PostCSSの場合は2を指定
               importLoaders: 2
             },
---
+++ tutorial-bootstrap-style/webpack.config.js
@@ -0,0 +1,60 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+// [定数] webpack の出力オプションを指定します
+// 'production' か 'development' を指定
+// cross-envを使って動的に値を変えたりしてもいいかもね
+const MODE = 'production';
+
+module.exports = {
+  // モード値を production に設定すると最適化された状態で、
+  // development に設定するとソースマップ有効でJSファイルが出力される
+  mode: MODE,
+  module: {
+    rules: [
+      {
+        // 対象となるファイルの拡張子(scss)
+        test: /\.scss$/,
+        // Sassファイルの読み込みとコンパイル
+        use: ExtractTextPlugin.extract([
+          // CSSをバンドルするための機能
+          {
+            loader: 'css-loader',
+            options: {
+              // オプションでCSS内のurl()メソッドの取り込まない
+              url: false,
+              // ソースマップの利用有無
+              sourceMap: true,
+              // 空白文字を取り除く
+              minimize: true,
+              // Sass+PostCSSの場合は2を指定
+              importLoaders: 2
+            },
+          },
+          // PostCSSのための設定
+          {
+            loader: 'postcss-loader',
+            options: {
+              // PostCSS側でもソースマップを有効にする
+              sourceMap: true,
+              // ベンダープレフィックスを自動付与する
+              plugins: () => [require('autoprefixer')]
+            },
+          },
+          // Sassをバンドルするための機能
+          {
+            loader: 'sass-loader',
+            options: {
+              // ソースマップの利用有無
+              sourceMap: true,
+            }
+          }
+        ]),
+      },
+    ],
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css'),
+  ],
+  // source-map方式でないと、CSSの元ソースが追跡できないため
+  devtool: "source-map"
+};
