bf8403a7f98039b382e2385fa70ea754436a1687
ics-ikeda <ikeda@ics-web.jp>
2018-04-09 18:13:53 +0900

    シンプルにBootstrapを使うためのサンプルを追加

---
+++ tutorial-bootstrap-min/webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  // モード値を production に設定すると最適化された状態で、
+  // development に設定するとソースマップ有効でJSファイルが出力される
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        // 対象となるファイルの拡張子(cssのみ)
+        test: /\.css$/,
+        // Sassファイルの読み込みとコンパイル
+        use: [
+          // スタイルシートをJSからlinkタグに展開する機能
+          'style-loader',
+          // CSSをバンドルするための機能
+          'css-loader',
+        ],
+      },
+    ],
+  }
+};
