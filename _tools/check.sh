b2e1c82c32276281154da1b0c145aaab62bff526
ics-ikeda <ikeda@ics-web.jp>
2018-05-16 14:54:34 +0900

    自動化ツールを整備

---
+++ _tools/check.sh
@@ -0,0 +1,43 @@
+# 更新したいプロジェクトフォルダーを定義
+declare -a array=(
+    "tutorial-babel"
+    "tutorial-babel-es2017"
+    "tutorial-babel-jquery"
+    "tutorial-babel-react"
+    "tutorial-babel-rxjs6"
+    "tutorial-babel-three"
+    "tutorial-babel-vue"
+    "tutorial-babel-zero_config"
+    "tutorial-bootstrap-min"
+    "tutorial-bootstrap-style"
+    "tutorial-bootstrap-style-js"
+    "tutorial-chunk"
+    "tutorial-chunkdist"
+    "tutorial-css"
+    "tutorial-css-extract-plugin"
+    "tutorial-js"
+    "tutorial-optimize"
+    "tutorial-optimize-aggressive"
+    "tutorial-sass"
+    "tutorial-sass-extract-plugin"
+    "tutorial-sass-image-file"
+    "tutorial-sass-image-url"
+    "tutorial-sass-image-url-limit"
+    "tutorial-sass-postcss"
+    "tutorial-typescript"
+    "tutorial-typescript-react"
+    "tutorial-typescript-three"
+    "tutorial-typescript-urlloader"
+    "tutorial-typescript-vue"
+    "tutorial-typescript-zero_config"
+    "tutorial-webpack-dev-server"
+    "tutorial-webpack-serve"
+)
+
+# とりあえず階層を潜っておく
+cd ../
+
+# 配列をまとめて実行
+for e in ${array[@]}; do
+    open ./${e}/dist/index.html
+done
