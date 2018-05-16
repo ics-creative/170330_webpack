697f5a872d3d03087c699d3a3f2180e1d2a8aebb
ics-ikeda <ikeda@ics-web.jp>
2018-05-16 15:09:09 +0900

    webpack-serveのサンプルを更新

--- tutorial-webpack-serve/webpack.config.js
+++ tutorial-webpack-serve/webpack.config.js
@@ -2,4 +2,10 @@ module.exports = {
   // モード値を production に設定すると最適化された状態で、
   // development に設定するとソースマップ有効でJSファイルが出力される
   mode: 'development',
+
+  // webpack-serve の設定
+  serve: {
+    open: true,
+    content: './dist'
+  }
 };
