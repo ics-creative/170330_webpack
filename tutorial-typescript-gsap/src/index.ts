d8f3af1e9dd47500269705a861366f5878f596b6
ics-ikeda <ikeda@ics-web.jp>
2018-06-01 20:37:53 +0900

    gpap + TypeScriptのサンプルを追加

---
+++ tutorial-typescript-gsap/src/index.ts
@@ -0,0 +1,29 @@
+import {Bounce, Cubic} from 'gsap/EasePack';
+// import {TweenLite, TimelineLite} from 'gsap'; // 容量がかさむ
+import TweenLite from 'gsap/TweenLite'; // 最小構成になる
+import TimelineLite from 'gsap/TimelineLite';
+
+// 適当に要素をたくさん用意する
+const rects = [];
+for (let i = 0; i < 50; i++) {
+  const rect = document.createElement('div');
+  rect.className = 'rect';
+  rect.style.left = `calc(100vw * ${Math.random()})`;
+  rect.style.top = `-100px`;
+  rect.style.transform = `rotateZ(${Math.random() * 360}deg)`;
+  document.body.appendChild(rect);
+  rects.push(rect);
+}
+
+// タイムラインを作成する
+const tl = new TimelineLite({repeat: -1, yoyo: true});
+rects.forEach((rect, index) => {
+  // トゥイーンインスタンスを追加する
+  tl.add(TweenLite.to(rect, 3, {
+    y        : window.innerHeight * 3 / 4,
+    rotationZ: 0,
+    ease     : Bounce.easeOut,
+    delay    : index * 0.02
+  }), 0);
+});
+
