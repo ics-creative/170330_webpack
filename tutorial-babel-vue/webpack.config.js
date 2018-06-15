8bccad9e2d1704280bf0a2c924db4d55d3ea0b61
ics-ikeda <ikeda@ics-web.jp>
2018-06-15 13:29:55 +0900

    ES2017→ES2018

--- tutorial-babel-vue/webpack.config.js
+++ tutorial-babel-vue/webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
         // Babel のオプションを指定する
         options: {
           presets: [
-            // env を指定することで、ES2017 を ES5 に変換。
+            // env を指定することで、ES2018 を ES5 に変換。
             // {modules: false}にしないと import 文が Babel によって CommonJS に変換され、
             // webpack の Tree Shaking 機能が使えない
             ['env', {'modules': false}]
