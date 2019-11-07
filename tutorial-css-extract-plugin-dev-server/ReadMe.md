7b87e4774efc4d99cecace3d754f84770b0c0282
ics-ikeda <ikeda@ics-web.jp>
2019-11-08 07:28:39 +0900

    webpack-dev-serverとCSS Extractの設定

---
+++ tutorial-css-extract-plugin-dev-server/ReadMe.md
@@ -0,0 +1,35 @@
+# WebPack + CSS with mini-css-extract-plugin のサンプル
+
+CSSをwebpackで取り込みつつ、cssファイルを出力するサンプルです。
+[mini-css-extract-plugin](https://github.com/webpack-contrib/mini-css-extract-plugin)の使い方を紹介します。
+
+
+## ゼロから環境構築する場合の手順
+
+### インストールコマンド
+
+まずはpackage.jsonを作る。
+
+```bash
+npm init -y
+```
+
+その上で、必要なモジュールをインストールする。
+
+```bash
+npm i -D webpack webpack-cli css-loader mini-css-extract-plugin
+```
+
+### webpack.config.js の書き方
+
+[webpack.config.js](webpack.config.js)のコードを参照ください。
+
+webpackのデフォルト構成としているので、`src`フォルダーにソースファイルを格納すること。デフォルトではエントリーポイントは`src/index.js`である。
+
+### ビルドコマンド
+
+```bash
+npx webpack
+```
+
+`dist`フォルダーに成果物が出力されます。
---
+++ tutorial-css-extract-plugin-dev-server/webpack.config.js
@@ -0,0 +1,33 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  // モード値を production に設定すると最適化された状態で、
+  // development に設定するとソースマップ有効でJSファイルが出力される
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          // CSSファイルを書き出すオプションを有効にする
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          // CSSを読み込む
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+  ],
+  // ローカル開発用環境を立ち上げる
+  // 実行時にブラウザが自動的に localhost を開く
+  devServer: {
+    contentBase: "dist",
+    open: true
+  }
+};
