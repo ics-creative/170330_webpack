4ee1d2dfc8be3b9ffda647ce66adde074ffb1c31
ics-ikeda <ikeda@ics-web.jp>
2020-03-25 17:39:57 +0900

    拡張子の整備

--- tutorial-typescript/webpack.config.js
+++ tutorial-typescript/webpack.config.js
@@ -12,14 +12,18 @@ module.exports = {
         // 拡張子 .ts の場合
         test: /\.ts$/,
         // TypeScript をコンパイルする
-        use: 'ts-loader'
-      }
-    ]
+        use: 'ts-loader',
+      },
+    ],
   },
   // import 文で .ts ファイルを解決するため
+  // これを定義しないと import 文で拡張子を書く必要が生まれる
+  // フロントエンドの開発では拡張子を省略することが多いので、
+  // 記載したほうがトラブルに巻き込まれにくい
   resolve: {
+    // 拡張子を配列で指定
     extensions: [
-      '.ts'
-    ]
-  }
+      '.ts', '.js',
+    ],
+  },
 };
