81e68f521add1b370d7a3b2df7a9ce78efef8580
ics-ikeda <ikeda@ics-web.jp>
2020-08-05 12:09:44 +0900

    Babel + GSAP : 作例をTSにあわせる

--- tutorial-babel-gsap/src/index.js
+++ tutorial-babel-gsap/src/index.js
@@ -1,13 +1,17 @@
 import gsap, {Bounce} from 'gsap';
 
+// 個数
+const MAX = 50;
+
 // 適当に要素をたくさん用意する
 const rects = [];
-for (let i = 0; i < 50; i++) {
-  const rect = document.createElement('div');
-  rect.className = 'rect';
-  rect.style.left = `calc(100vw * ${Math.random()})`;
+for (let i = 0; i < MAX; i++) {
+  const rect = document.createElement("div");
+  rect.className = "rect";
+  rect.style.width = `calc(100vw * ${1 / MAX})`;
+  rect.style.height = `calc(100vw * ${1 / MAX})`;
+  rect.style.left = `calc(100vw * ${i / MAX})`;
   rect.style.top = `-100px`;
-  rect.style.transform = `rotate(${Math.random() * 360}deg)`;
   document.body.appendChild(rect);
   rects.push(rect);
 }
@@ -16,11 +20,10 @@ for (let i = 0; i < 50; i++) {
 const tl = gsap.timeline({repeat: -1, repeatDelay: 1});
 rects.forEach((rect, index) => {
   // トゥイーンインスタンスを追加する
-  tl.add(gsap.to(rect, 3, {
+  tl.add(gsap.to(rect, {
+    duration: 3,
     y: window.innerHeight * 3 / 4,
-    rotate: 0,
     ease: Bounce.easeOut,
-    delay: index * 0.02,
+    delay: Math.random()
   }), 0);
 });
-
